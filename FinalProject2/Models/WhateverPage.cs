30e6b03eced99727d0b84db5a638c318ad15e4d3
Chanise Hart <niecy23@gmail.com>
2023-08-03 13:42:43 -0700

    Last Updates Before Data Recovery Issue

--- FinalProject2/Models/WhateverPage.cs
+++ FinalProject2/Models/WhateverPage.cs
@@ -1,31 +1,31 @@
-﻿using System;
-namespace FinalProject2.Models
-{
-    public class WhateverPage
-    {
-        public WhateverPage(int id)
-        {
-            Event instance = new Event();
-            User user = new User();
-        }
+﻿//using System;
+//namespace FinalProject2.Models
+//{
+//    public class WhateverPage
+//    {
+//        public WhateverPage(int id)
+//        {
+//            Event instance = new Event();
+//            User user = new User();
+//        }
 
 
 
-        public int EventID { get; set; }
-        public string EventName { get; set; }
-        public DateTime DateAndTime { get; set; }
-        public string Location { get; set; }
-        public string Description { get; set; }
+//        public int EventID { get; set; }
+//        public string EventName { get; set; }
+//        public DateTime DateAndTime { get; set; }
+//        public string Location { get; set; }
+//        public string Description { get; set; }
 
-        public int UserID { get; set; }
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string EmailAddress { get; set; } = string.Empty;
-        public string PhoneNumber { get; set; } = string.Empty;
-        public IEnumerable<Event> Events { get; set; }
+//        public int UserID { get; set; }
+//        public string FirstName { get; set; } = string.Empty;
+//        public string LastName { get; set; } = string.Empty;
+//        public string EmailAddress { get; set; } = string.Empty;
+//        public string PhoneNumber { get; set; } = string.Empty;
+//        public IEnumerable<Event> Events { get; set; }
 
 
 
-    }
-}
+//    }
+//}
 
